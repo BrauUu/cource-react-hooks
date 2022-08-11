8b9d4810a1d91016be091ba13a71150869d3e4d8
Brayan Frank <brayanfrank.sm@gmail.com>
2022-08-10 23:09:11 -0300

    using contextAPI and useContext

---
+++ src/data/DataContext.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const data = {
+    number: 41,
+    string: 'Hi mom',
+    boolean: false
+}
+
+export const DataContext = React.createContext(data)
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,18 +1,23 @@
 import './App.css'
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { data, DataContext } from '../data/DataContext'
 
 import Menu from '../components/layout/Menu'
 import Content from '../components/layout/Content'
 
 const App = props => {
 
+    const [state, setState] = useState(data)
+
     return (
         <div className="App">
-            <Router>
-                <Menu />
-                <Content />
-            </Router>
+            <DataContext.Provider value={{state, setState}}>
+                <Router>
+                    <Menu />
+                    <Content />
+                </Router>
+            </DataContext.Provider>
         </div>
     )
 }
